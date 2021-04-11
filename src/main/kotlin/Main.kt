c05e2d5cb86ed9fe715eb90c4e8b07a9d35dd140
shmuel <shmuelsternbach613@gmail.com>
2021-04-11 18:22:40 -0400

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+/*function finder($add,$product)
+{
+
+ $inside_root = $add*$add - 4*$product;
+
+ if($inside_root >=0)
+ {
+
+     $b = ($add + sqrt($inside_root))/2;
+     $a = $add - $b;
+
+     echo "$a+$b = $add and $a*$b=$product\n";
+
+ }else
+ {
+   echo "No real solution\n";
+ }
+}*/
